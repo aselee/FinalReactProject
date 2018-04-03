b8c1e8516278a5e3ed296809fdce99a61ad44b37
Andrew <32229265+aselee@users.noreply.github.com>
2018-04-03 17:58:43 -0500

    App js

--- reactfinal/src/App.js
+++ reactfinal/src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from './Titlebar.js';
+import {orange500} from 'material-ui/styles/colors';
+import Card from './Cardcoin.js';
+
+const muiTheme = getMuiTheme({
+  appBar: {
+    height: 50,
+    color: orange500,
+  },
+});
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <AppBar title="Cryptocurrency"/>
+            <Card />
+        </MuiThemeProvider>
       </div>
     );
   }
